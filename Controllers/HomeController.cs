370c859c7b75face218c911407964aadb1e0ac3b
Soma Mukherjee <Soma Mukherjee@SOMA>
2022-09-30 13:58:41 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactDotnetCoreNew.Models;
+using System.Diagnostics;
+
+namespace ReactDotnetCoreNew.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>();
+            
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_comments);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("comments")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+        [Route("comments/new")]
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            comment.Id = _comments.Count + 1;
+            _comments.Add(comment);
+            return Content("Success");
+        }
+    }
+}
